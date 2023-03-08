d0cd5e168f417546010672310dafb600ca61e762
jhkyun <jhkyun@osci.kr>
2023-03-08 20:07:55 +0900

    modify google login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,19 +6,11 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Get('login/google')
+  @Get('/login/google')
   @UseGuards(AuthGuard('google'))
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async googleLogin(): Promise<void> {}
-
-  @Get('callback/google')
-  @UseGuards(AuthGuard('google'))
-  async googleCallback(
+  async googleLogin(
     @Req() req,
   ): Promise<{ accessToken: string; refreshToken: string }> {
-    const user = await this.authService.findUserByEmailOrSave(req.user);
-    const { accessToken, refreshToken } = this.authService.createToken(user);
-    await this.authService.updateHashedRefreshToken(user.id, refreshToken);
-    return { accessToken, refreshToken };
+    return this.authService.loginOrSignIn(req.user);
   }
 }
