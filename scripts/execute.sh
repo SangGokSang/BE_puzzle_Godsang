32482eff62f0a0219fdc2441e0bcbc9f13cf5bf2
jhkyun <jhkyun@osci.kr>
2023-03-25 12:31:26 +0900

    wip

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/dm2023-nest-app
+cd /home/ubuntu/dm2023-nest-app || exit
 
-DOCKER_IMAGE_NAME=dm2023-nest-app
-
-DOCKER_CONTAINER_NAME=nest-app
-
-docker build -t ${DOCKER_IMAGE_NAME} ../
-
-docker run -d -p 3000:3000 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+sudo docker-compose up -d
