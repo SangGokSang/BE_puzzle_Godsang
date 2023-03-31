2af88ee4ec3464f92fb0611715825a2062018109
jhkyun <jhkyun@osci.kr>
2023-03-31 12:14:59 +0900

    url 변경

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,6 +2,6 @@
 
 cd /home/ubuntu/dm2023-nest || exit
 
-sudo docker stop dm2023-nest
-sudo docker rm dm2023-nest
-sudo docker rmi dm2023-nest
+sudo docker stop dm2023-nest || exit
+sudo docker rm dm2023-nest  || exit
+sudo docker rmi dm2023-nest || exit
