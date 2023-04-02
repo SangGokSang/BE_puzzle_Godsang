e5c34beacdcba11fd83a83a49bad7d2c8e4863ff
jhkyun <jhkyun@osci.kr>
2023-04-02 14:40:38 +0900

    프로비지넝시 mysql client 설치

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -9,3 +9,5 @@ sudo usermod -aG docker "${USER}"
 
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt install -y mysql-client
