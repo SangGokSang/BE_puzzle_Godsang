2bb544263b88341e8480ccddfdab188bc3d42003
jhkyun <jhkyun@osci.kr>
2023-04-21 16:27:45 +0900

    카테고리 추가

--- src/puzzle/dto/puzzle-create.dto.ts
+++ src/puzzle/dto/puzzle-create.dto.ts
@@ -1,7 +1,15 @@
 import { IsIn, Length, Matches } from 'class-validator';
 
 export class PuzzleCreateDto {
-  @IsIn(['EXERCISE', 'CAREER', 'TRAVEL', 'MONEY_MANAGEMENT', 'ETC'])
+  @IsIn([
+    'EXERCISE',
+    'CAREER',
+    'TRAVEL',
+    'MONEY_MANAGEMENT',
+    'QUITTING_SMOKING',
+    'LOVE',
+    'ETC',
+  ])
   category: string;
 
   @Matches(/^[^;]*$/)
