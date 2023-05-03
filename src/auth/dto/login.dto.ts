5e38c9f7f26b75b690db16fa6a8fb740350e8485
jhkyun <jhkyun@osci.kr>
2023-05-03 14:07:49 +0900

    로그인 유효성 검사 제거

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,11 @@
-import { IsIn } from 'class-validator';
+import { IsEmail, IsIn, Length } from 'class-validator';
 
 export class LoginDto {
   @IsIn(['kakao', 'naver'])
   provider: string;
   providerId: string;
+  @Length(1, 10)
   nickname: string;
+  @IsEmail()
   email: string;
 }
